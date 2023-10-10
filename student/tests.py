0bd85cb3f72f819af7820dc19ccbee326ef4a80f
Yers <eabuov4@gmail.com>
2023-10-10 22:26:35 +0600

    Integrating database testing

--- student/tests.py
+++ student/tests.py
@@ -2,8 +2,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from student.models import Student
+
 """
 ---UNIT TEST---
+
 class StudentViewsTestCase(TestCase):
 
     def setUp(self):
@@ -34,9 +36,9 @@ class StudentViewsTestCase(TestCase):
         response = self.client.post(reverse('student:student_delete', args=[self.student.pk]))
         self.assertEqual(response.status_code, 302)  # Expecting a redirect
         self.assertFalse(Student.objects.filter(pk=self.student.pk).exists())
-"""
-"""
+
 ---INTEGRATION TEST---
+
 class StudentViewsIntegrationTestCase(TestCase):
 
     def test_student_crud_workflow(self):
@@ -68,8 +70,9 @@ class StudentViewsIntegrationTestCase(TestCase):
         response = self.client.post(delete_url)
         self.assertEqual(response.status_code, 302)  # Expecting a redirect
         self.assertFalse(Student.objects.filter(pk=student.pk).exists())
-"""
-#SMOKE TEST
+
+---SMOKE TEST---
+
 class SmokeTest(TestCase):
     def setUp(self):
         self.student = Student.objects.create(name="Test Student", section="A", age=20)
@@ -108,4 +111,76 @@ class SmokeTest(TestCase):
         self.assertFalse(Student.objects.filter(pk=self.student.pk).exists())
 
     def tearDown(self):
-        self.student.delete()
+        self.student.delete() 
+"""
+class StudentModelTestCase(TestCase):
+    def setUp(self):
+        self.student = Student.objects.create(
+            name="John Doe",
+            section="A",
+            age=20
+        )
+
+    def test_student_creation(self):
+        self.assertEqual(self.student.name, "John Doe")
+        self.assertEqual(self.student.section, "A")
+        self.assertEqual(self.student.age, 20)
+
+    def test_student_absolute_url(self):
+        expected_url = reverse('student:student_edit', kwargs={'pk': self.student.pk})
+        self.assertEqual(self.student.get_absolute_url(), expected_url)
+
+class StudentListViewTestCase(TestCase):
+    def test_student_list_view(self):
+        response = self.client.get(reverse('student:student_list'))
+        self.assertEqual(response.status_code, 200)
+        # You can add more assertions here to check the content of the response.
+
+class StudentCreateViewTestCase(TestCase):
+    def test_student_create_view(self):
+        response = self.client.post(reverse('student:student_new'), data={
+            'name': 'New Student',
+            'section': 'B',
+            'age': 22,
+        })
+        self.assertEqual(response.status_code, 302)  # Check if the response is a redirect
+
+        # Check if the student was created in the database
+        self.assertTrue(Student.objects.filter(name='New Student', section='B', age=22).exists())
+
+class StudentUpdateViewTestCase(TestCase):
+    def setUp(self):
+        self.student = Student.objects.create(
+            name="John Doe",
+            section="A",
+            age=20
+        )
+
+    def test_student_update_view(self):
+        response = self.client.post(reverse('student:student_edit', kwargs={'pk': self.student.pk}), data={
+            'name': 'Updated Student',
+            'section': 'C',
+            'age': 25,
+        })
+        self.assertEqual(response.status_code, 302)  # Check if the response is a redirect
+
+        # Check if the student was updated in the database
+        updated_student = Student.objects.get(pk=self.student.pk)
+        self.assertEqual(updated_student.name, 'Updated Student')
+        self.assertEqual(updated_student.section, 'C')
+        self.assertEqual(updated_student.age, 25)
+
+class StudentDeleteViewTestCase(TestCase):
+    def setUp(self):
+        self.student = Student.objects.create(
+            name="John Doe",
+            section="A",
+            age=20
+        )
+
+    def test_student_delete_view(self):
+        response = self.client.post(reverse('student:student_delete', kwargs={'pk': self.student.pk}))
+        self.assertEqual(response.status_code, 302)  # Check if the response is a redirect
+
+        # Check if the student was deleted from the database
+        self.assertFalse(Student.objects.filter(pk=self.student.pk).exists())
